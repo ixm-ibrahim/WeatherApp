d40dddb9d7b5fe0d27f56bf1a726fbbb9a171b34
jaafar rodgers <95446026+MrFiszbi1@users.noreply.github.com>
2023-08-17 15:16:15 -0700

    no logo
    react logo removed

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -13,7 +13,6 @@ const HomePage = () => {
     return (
     <div>
         <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
             <p>
             Edit <code>src/App.js</code> and save to reload.
             </p>
