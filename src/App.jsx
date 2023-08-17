d00ae1f5c687d8617fb0a6c72c275f79441d084c
jaafar rodgers <95446026+MrFiszbi1@users.noreply.github.com>
2023-08-17 15:14:49 -0700

    App code to home page
    Code that was in App.js is now in the home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,10 @@
-import logo from './assets/images/logo.svg';
 import './assets/css/App.css';
-import { ControlsComponent } from './components/ControlsComponent.jsx';
-import { useRef } from "react";
-import { BoxGraphic, ShadersGraphic } from './components/WeatherGraphics';
-import { Canvas, useFrame } from '@react-three/fiber'
+import HomePage from './pages/HomePage';
 
 function App() {
-  const canvasRef = useRef(null);
-  //const canvasRef = React.createRef();
-  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <div>
-        <ControlsComponent />
-      </div>
-      <Canvas>
-        <ambientLight />
-        <pointLight position={[10, 10, 10]} />
-        <BoxGraphic position={[-1.2, 0, 0]} />
-        <BoxGraphic position={[1.2, 0, 0]} />
-      </Canvas>
-      <div ref={canvasRef}>
-        <Canvas camera={{ position: [0.0, 0.0, 1.0] }}>
-          <ShadersGraphic canvasRef={canvasRef}/>
-        </Canvas>
-      </div>
+     <HomePage></HomePage>
     </div>
   );
 }
