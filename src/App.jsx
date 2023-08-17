1a0c1595882e6c908f1a6a6a9bd29f5a916051e1
jaafar rodgers <95446026+MrFiszbi1@users.noreply.github.com>
2023-08-17 15:00:25 -0700

    organization and dependencies
    page organization and more dependencies added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './assets/images/logo.svg';
+import './assets/css/App.css';
 import { ControlsComponent } from './components/ControlsComponent.jsx';
 import { useRef } from "react";
 import { BoxGraphic, ShadersGraphic } from './components/WeatherGraphics';
